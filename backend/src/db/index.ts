e858adaf36df407ab018c7fe7f519e630e260bbf
arnoldpineda <arnold.pineda.rojas@gmail.com>
2024-06-16 18:04:47 -0500

    style: :coffin: remove unused code

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,10 +4,8 @@ import Comment from './models/Comment';
 import User from './models/User';
 import Favorite from './models/Favorite';
 
-import sequelize from '../config/database'; // Importa la instancia de Sequelize configurada
+import sequelize from '../config/database';
 
-// Agregar los modelos a la instancia de Sequelize
 sequelize.addModels([Character, Specie, Comment, User, Favorite]);
 
-// Exporta la instancia de Sequelize y los modelos
 export { sequelize, Character, Specie, Comment, User, Favorite };
